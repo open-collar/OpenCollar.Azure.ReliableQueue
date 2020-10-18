2c1d262d92d152e3ddfa01051f61fa974db2686e
Jonathan Evans <jevans@open-collar.org.uk>
2020-10-18 20:29:33 +0100

    Further unit tests.

--- test/OpenCollar.Azure.ReliableQueue.TESTS/Model/QueueKeyTests.cs
+++ test/OpenCollar.Azure.ReliableQueue.TESTS/Model/QueueKeyTests.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 using Xunit;
 
@@ -8,6 +6,36 @@ namespace OpenCollar.Azure.ReliableQueue.TESTS.Model
 {
     public sealed class QueueKeyTests
     {
+        [Fact]
+        public void TestCompareTo()
+        {
+            var a = new OpenCollar.Azure.ReliableQueue.Model.QueueKey("AAA");
+            var a1 = new OpenCollar.Azure.ReliableQueue.Model.QueueKey("AAA");
+            var b = new OpenCollar.Azure.ReliableQueue.Model.QueueKey("BBB");
+            var c = new OpenCollar.Azure.ReliableQueue.Model.QueueKey("CCC");
+
+            Assert.Equal(1, a.CompareTo((OpenCollar.Azure.ReliableQueue.Model.QueueKey)null));
+            Assert.Equal(0, a.CompareTo(a));
+            Assert.Equal(0, a.CompareTo(a1));
+            Assert.Equal(-1, a.CompareTo(b));
+            Assert.Equal(1, b.CompareTo(a));
+        }
+
+        [Fact]
+        public void TestCompareToObject()
+        {
+            var a = new OpenCollar.Azure.ReliableQueue.Model.QueueKey("AAA");
+            var a1 = new OpenCollar.Azure.ReliableQueue.Model.QueueKey("AAA");
+            var b = new OpenCollar.Azure.ReliableQueue.Model.QueueKey("BBB");
+            var c = new OpenCollar.Azure.ReliableQueue.Model.QueueKey("CCC");
+
+            Assert.Equal(1, a.CompareTo((object)null));
+            Assert.Equal(0, a.CompareTo((object)a));
+            Assert.Equal(0, a.CompareTo((object)a1));
+            Assert.Equal(-1, a.CompareTo((object)b));
+            Assert.Equal(1, b.CompareTo((object)a));
+        }
+
         [Fact]
         public void TestConstructor()
         {
@@ -20,7 +48,21 @@ namespace OpenCollar.Azure.ReliableQueue.TESTS.Model
             Assert.Equal("test-name-1", x.Identifier);
             Assert.Equal("TestxNamex1", x.TableIdentifier);
             Assert.Equal("TEST+NAME+1", x.ToString());
+        }
 
+        [Fact]
+        public void TestEquals()
+        {
+            var a = new OpenCollar.Azure.ReliableQueue.Model.QueueKey("AAA");
+            var a1 = new OpenCollar.Azure.ReliableQueue.Model.QueueKey("AAA");
+            var b = new OpenCollar.Azure.ReliableQueue.Model.QueueKey("BBB");
+            var c = new OpenCollar.Azure.ReliableQueue.Model.QueueKey("CCC");
+
+            Assert.False(a.Equals((OpenCollar.Azure.ReliableQueue.Model.QueueKey)null));
+            Assert.True(a.Equals(a));
+            Assert.True(a.Equals(a1));
+            Assert.False(a.Equals(22));
+            Assert.False(a.Equals(b));
         }
     }
 }
