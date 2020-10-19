221100357afe3128a15b1ca29aea3bb000c67f76
Jonathan Evans <jevans@open-collar.org.uk>
2020-10-19 08:04:45 +0100

    Added unit tests for Topic type.  Fixed wanrings.

--- src/OpenCollar.Azure.ReliableQueue/Model/Topic.cs
+++ src/OpenCollar.Azure.ReliableQueue/Model/Topic.cs
@@ -22,52 +22,54 @@ namespace OpenCollar.Azure.ReliableQueue.Model
     using System;
     using System.Collections.Generic;
 
-    using JetBrains.Annotations;
-
     using global::Newtonsoft.Json;
+
+    using JetBrains.Annotations;
 
     using OpenCollar.Azure.ReliableQueue.Model.Newtonsoft;
 
     /// <summary>
-    /// Defines the <see cref="Topic" />.
+    ///     Defines the <see cref="Topic" />.
     /// </summary>
     [JsonConverter(typeof(TopicConverter))]
     [System.Text.Json.Serialization.JsonConverter(typeof(Text.Json.TopicConverter))]
     public sealed class Topic : IEquatable<Topic>, IComparable<Topic>, IComparable
     {
         /// <summary>
-        /// Defines the DefaultIdentifier.
+        ///     Defines the DefaultIdentifier.
         /// </summary>
         [NotNull]
-        private const string DefaultIdentifier = "__default__";
+        public const string DefaultIdentifier = "__default__";
 
         /// <summary>
-        /// Defines the Default.
+        ///     Defines the Default.
         /// </summary>
         [NotNull]
-        public static readonly Topic Default = new Topic(DefaultIdentifier);
+        public static readonly Topic Default = new Topic(null);
 
         /// <summary>
-        /// Defines the _identifier.
+        ///     Defines the _identifier.
         /// </summary>
         [NotNull]
         private readonly string _identifier;
 
         /// <summary>
-        /// The key that uniquely identifies the reliable queue in the configuration.  The key is case insensitive.  The key must not be
-        ///     <see langword="null"/>, zero-length or contain only white-space characters..
+        ///     The key that uniquely identifies the reliable queue in the configuration. The key is case insensitive.
+        ///     The key must not be <see langword="null" />, zero-length or contain only white-space characters..
         /// </summary>
         [CanBeNull]
         private readonly string? _key;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="Topic"/> class.
+        ///     Initializes a new instance of the <see cref="Topic" /> class.
         /// </summary>
-        /// <param name="key">The key<see cref="string"/>.</param>
+        /// <param name="key">
+        ///     The key <see cref="string" />.
+        /// </param>
         [JsonConstructor]
-        public Topic([CanBeNull] string key)
+        public Topic([CanBeNull] string? key)
         {
-            if (string.IsNullOrWhiteSpace(key))
+            if(string.IsNullOrWhiteSpace(key))
             {
                 _key = null;
                 _identifier = DefaultIdentifier;
@@ -76,36 +78,43 @@ namespace OpenCollar.Azure.ReliableQueue.Model
             {
                 _key = key;
 #pragma warning disable CA1308 // Normalize strings to uppercase
+#pragma warning disable CS8601 // Possible null reference assignment.
                 _identifier = Identifiers.MakeSafe(_key.ToLowerInvariant());
+#pragma warning restore CS8601 // Possible null reference assignment.
 #pragma warning restore CA1308 // Normalize strings to uppercase
             }
         }
 
         /// <summary>
-        /// Gets the Identifier.
+        ///     Gets the Identifier.
         /// </summary>
         [NotNull]
         public string Identifier => _identifier;
 
         /// <summary>
-        /// Gets a value indicating whether IsEmpty.
+        ///     Gets a value indicating whether IsEmpty.
         /// </summary>
         public bool IsEmpty => _key is null;
 
         /// <summary>
-        /// Compares the current instance with another object of the same type and returns an integer that indicates whether the current instance precedes,
-        ///     follows, or occurs in the same position in the sort order as the other object.
+        ///     Compares the current instance with another object of the same type and returns an integer that indicates
+        ///     whether the current instance precedes, follows, or occurs in the same position in the sort order as the
+        ///     other object.
         /// </summary>
-        /// <param name="obj">An object to compare with this instance.</param>
-        /// <returns>The <see cref="int"/>.</returns>
+        /// <param name="obj">
+        ///     An object to compare with this instance.
+        /// </param>
+        /// <returns>
+        ///     The <see cref="int" />.
+        /// </returns>
         public int CompareTo(object obj)
         {
-            if (ReferenceEquals(null, obj))
+            if(ReferenceEquals(null, obj))
             {
                 return 1;
             }
 
-            if (ReferenceEquals(this, obj))
+            if(ReferenceEquals(this, obj))
             {
                 return 0;
             }
@@ -114,19 +123,24 @@ namespace OpenCollar.Azure.ReliableQueue.Model
         }
 
         /// <summary>
-        /// Compares the current instance with another object of the same type and returns an integer that indicates whether the current instance precedes,
-        ///     follows, or occurs in the same position in the sort order as the other object.
+        ///     Compares the current instance with another object of the same type and returns an integer that indicates
+        ///     whether the current instance precedes, follows, or occurs in the same position in the sort order as the
+        ///     other object.
         /// </summary>
-        /// <param name="other">An object to compare with this instance.</param>
-        /// <returns>The <see cref="int"/>.</returns>
+        /// <param name="other">
+        ///     An object to compare with this instance.
+        /// </param>
+        /// <returns>
+        ///     The <see cref="int" />.
+        /// </returns>
         public int CompareTo(Topic other)
         {
-            if (ReferenceEquals(this, other))
+            if(ReferenceEquals(this, other))
             {
                 return 0;
             }
 
-            if (ReferenceEquals(null, other))
+            if(ReferenceEquals(null, other))
             {
                 return 1;
             }
@@ -135,25 +149,34 @@ namespace OpenCollar.Azure.ReliableQueue.Model
         }
 
         /// <summary>
-        /// The Equals.
+        ///     The Equals.
         /// </summary>
-        /// <param name="obj">The object to compare with the current object.</param>
-        /// <returns><see langword="true"/> if the specified object  is equal to the current object; otherwise, <see langword="false"/>.</returns>
+        /// <param name="obj">
+        ///     The object to compare with the current object.
+        /// </param>
+        /// <returns>
+        ///     <see langword="true" /> if the specified object is equal to the current object; otherwise, <see langword="false" />.
+        /// </returns>
         public override bool Equals(object obj) => ReferenceEquals(this, obj) || obj is Topic other && Equals(other);
 
         /// <summary>
-        /// The Equals.
+        ///     The Equals.
         /// </summary>
-        /// <param name="other">An object to compare with this object.</param>
-        /// <returns><see langword="true"/> if the current object is equal to the <paramref name="other"/> parameter; otherwise, <see langword="false"/>.</returns>
+        /// <param name="other">
+        ///     An object to compare with this object.
+        /// </param>
+        /// <returns>
+        ///     <see langword="true" /> if the current object is equal to the <paramref name="other" /> parameter;
+        ///     otherwise, <see langword="false" />.
+        /// </returns>
         public bool Equals(Topic other)
         {
-            if (ReferenceEquals(null, other))
+            if(ReferenceEquals(null, other))
             {
                 return false;
             }
 
-            if (ReferenceEquals(this, other))
+            if(ReferenceEquals(this, other))
             {
                 return true;
             }
@@ -162,79 +185,138 @@ namespace OpenCollar.Azure.ReliableQueue.Model
         }
 
         /// <summary>
-        /// The GetHashCode.
+        ///     The GetHashCode.
         /// </summary>
-        /// <returns>A hash code for the current object.</returns>
+        /// <returns>
+        ///     A hash code for the current object.
+        /// </returns>
         public override int GetHashCode() => StringComparer.OrdinalIgnoreCase.GetHashCode(_identifier);
 
         /// <summary>
-        /// The ToString.
+        ///     The ToString.
         /// </summary>
-        /// <returns>A string that represents the current object.</returns>
+        /// <returns>
+        ///     A string that represents the current object.
+        /// </returns>
         public override string ToString() => _key;
 
-
-        /// <summary>Returns a value that indicates whether the values of two <see cref="OpenCollar.Azure.ReliableQueue.Model.Topic"/> objects are equal.</summary>
-        /// <param name="left">The first value to compare.</param>
-        /// <param name="right">The second value to compare.</param>
+        /// <summary>
+        ///     Returns a value that indicates whether the values of two
+        ///     <see cref="OpenCollar.Azure.ReliableQueue.Model.Topic" /> objects are equal.
+        /// </summary>
+        /// <param name="left">
+        ///     The first value to compare.
+        /// </param>
+        /// <param name="right">
+        ///     The second value to compare.
+        /// </param>
         /// <returns>
-        ///     <see langword="true"/> if the <paramref name="left"/> and <paramref name="right"/> parameters have the same value; otherwise,
-        ///     <see langword="false"/>.
+        ///     <see langword="true" /> if the <paramref name="left" /> and <paramref name="right" /> parameters have
+        ///     the same value; otherwise, <see langword="false" />.
         /// </returns>
         public static bool operator ==(Topic left, Topic right) => Equals(left, right);
 
         /// <summary>
-        ///     Returns a value that indicates whether a <see cref="OpenCollar.Azure.ReliableQueue.Model.Topic"/> value is greater than another
-        ///     <see cref="OpenCollar.Azure.ReliableQueue.Model.Topic"/> value.
+        ///     Returns a value that indicates whether a <see cref="OpenCollar.Azure.ReliableQueue.Model.Topic" /> value
+        ///     is greater than another <see cref="OpenCollar.Azure.ReliableQueue.Model.Topic" /> value.
         /// </summary>
-        /// <param name="left">The first value to compare.</param>
-        /// <param name="right">The second value to compare.</param>
-        /// <returns><see langword="true"/> if <paramref name="left"/> is greater than <paramref name="right"/>; otherwise, <see langword="false"/>.</returns>
+        /// <param name="left">
+        ///     The first value to compare.
+        /// </param>
+        /// <param name="right">
+        ///     The second value to compare.
+        /// </param>
+        /// <returns>
+        ///     <see langword="true" /> if <paramref name="left" /> is greater than <paramref name="right" />;
+        ///     otherwise, <see langword="false" />.
+        /// </returns>
         public static bool operator >(Topic left, Topic right) => Comparer<Topic>.Default.Compare(left, right) > 0;
 
         /// <summary>
-        ///     Returns a value that indicates whether a <see cref="OpenCollar.Azure.ReliableQueue.Model.Topic"/> value is greater than or equal to another
-        ///     <see cref="OpenCollar.Azure.ReliableQueue.Model.Topic"/> value.
+        ///     Returns a value that indicates whether a <see cref="OpenCollar.Azure.ReliableQueue.Model.Topic" /> value
+        ///     is greater than or equal to another <see cref="OpenCollar.Azure.ReliableQueue.Model.Topic" /> value.
         /// </summary>
-        /// <param name="left">The first value to compare.</param>
-        /// <param name="right">The second value to compare.</param>
-        /// <returns><see langword="true"/> if <paramref name="left"/> is greater than <paramref name="right"/>; otherwise, <see langword="false"/>.</returns>
+        /// <param name="left">
+        ///     The first value to compare.
+        /// </param>
+        /// <param name="right">
+        ///     The second value to compare.
+        /// </param>
+        /// <returns>
+        ///     <see langword="true" /> if <paramref name="left" /> is greater than <paramref name="right" />;
+        ///     otherwise, <see langword="false" />.
+        /// </returns>
         public static bool operator >=(Topic left, Topic right) => Comparer<Topic>.Default.Compare(left, right) >= 0;
 
-        /// <summary>Performs an implicit conversion from <see cref="Topic"/> to <see cref="string"/>.</summary>
-        /// <param name="value">The value to convert.</param>
-        /// <returns>The result of the conversion.</returns>
+        /// <summary>
+        ///     Performs an implicit conversion from <see cref="Topic" /> to <see cref="string" />.
+        /// </summary>
+        /// <param name="value">
+        ///     The value to convert.
+        /// </param>
+        /// <returns>
+        ///     The result of the conversion.
+        /// </returns>
         [CanBeNull]
         public static implicit operator string(Topic value) => value?._key;
 
-        /// <summary>Performs an implicit conversion from <see cref="string"/> to <see cref="Topic"/>.</summary>
-        /// <param name="value">The value to convert.</param>
-        /// <returns>The result of the conversion.</returns>
+        /// <summary>
+        ///     Performs an implicit conversion from <see cref="string" /> to <see cref="Topic" />.
+        /// </summary>
+        /// <param name="value">
+        ///     The value to convert.
+        /// </param>
+        /// <returns>
+        ///     The result of the conversion.
+        /// </returns>
         [NotNull]
         public static implicit operator Topic([CanBeNull] string value) => value is null ? Default : new Topic(value);
 
-        /// <summary>Returns a value that indicates whether two <see cref="OpenCollar.Azure.ReliableQueue.Model.Topic"/> objects have different values.</summary>
-        /// <param name="left">The first value to compare.</param>
-        /// <param name="right">The second value to compare.</param>
-        /// <returns><see langword="true"/> if <paramref name="left"/> and <paramref name="right"/> are not equal; otherwise, <see langword="false"/>.</returns>
+        /// <summary>
+        ///     Returns a value that indicates whether two <see cref="OpenCollar.Azure.ReliableQueue.Model.Topic" />
+        ///     objects have different values.
+        /// </summary>
+        /// <param name="left">
+        ///     The first value to compare.
+        /// </param>
+        /// <param name="right">
+        ///     The second value to compare.
+        /// </param>
+        /// <returns>
+        ///     <see langword="true" /> if <paramref name="left" /> and <paramref name="right" /> are not equal;
+        ///     otherwise, <see langword="false" />.
+        /// </returns>
         public static bool operator !=(Topic left, Topic right) => !Equals(left, right);
 
         /// <summary>
-        ///     Returns a value that indicates whether a <see cref="OpenCollar.Azure.ReliableQueue.Model.Topic"/> value is less than another
-        ///     <see cref="OpenCollar.Azure.ReliableQueue.Model.Topic"/> value.
+        ///     Returns a value that indicates whether a <see cref="OpenCollar.Azure.ReliableQueue.Model.Topic" /> value
+        ///     is less than another <see cref="OpenCollar.Azure.ReliableQueue.Model.Topic" /> value.
         /// </summary>
-        /// <param name="left">The first value to compare.</param>
-        /// <param name="right">The second value to compare.</param>
-        /// <returns><see langword="true"/> if <paramref name="left"/> is less than <paramref name="right"/>; otherwise, <see langword="false"/>.</returns>
+        /// <param name="left">
+        ///     The first value to compare.
+        /// </param>
+        /// <param name="right">
+        ///     The second value to compare.
+        /// </param>
+        /// <returns>
+        ///     <see langword="true" /> if <paramref name="left" /> is less than <paramref name="right" />; otherwise, <see langword="false" />.
+        /// </returns>
         public static bool operator <(Topic left, Topic right) => Comparer<Topic>.Default.Compare(left, right) < 0;
 
         /// <summary>
-        ///     Returns a value that indicates whether a <see cref="OpenCollar.Azure.ReliableQueue.Model.Topic"/> value is less than or equal to another
-        ///     <see cref="OpenCollar.Azure.ReliableQueue.Model.Topic"/> value.
+        ///     Returns a value that indicates whether a <see cref="OpenCollar.Azure.ReliableQueue.Model.Topic" /> value
+        ///     is less than or equal to another <see cref="OpenCollar.Azure.ReliableQueue.Model.Topic" /> value.
         /// </summary>
-        /// <param name="left">The first value to compare.</param>
-        /// <param name="right">The second value to compare.</param>
-        /// <returns><see langword="true"/> if <paramref name="left"/> is less than or equal to <paramref name="right"/>; otherwise, <see langword="false"/>.</returns>
+        /// <param name="left">
+        ///     The first value to compare.
+        /// </param>
+        /// <param name="right">
+        ///     The second value to compare.
+        /// </param>
+        /// <returns>
+        ///     <see langword="true" /> if <paramref name="left" /> is less than or equal to <paramref name="right" />;
+        ///     otherwise, <see langword="false" />.
+        /// </returns>
         public static bool operator <=(Topic left, Topic right) => Comparer<Topic>.Default.Compare(left, right) <= 0;
     }
 }
---
+++ test/OpenCollar.Azure.ReliableQueue.TESTS/Model/TopicTests.cs
@@ -0,0 +1,145 @@
+﻿using System;
+
+using Xunit;
+
+namespace OpenCollar.Azure.ReliableQueue.TOPICS.Model
+{
+    public sealed class TopicTests
+    {
+        [Fact]
+        public void TestCastOperands()
+        {
+            var a = new OpenCollar.Azure.ReliableQueue.Model.Topic("AAA");
+
+            Assert.Equal(a, "AAA");
+            Assert.Equal("AAA", a);
+
+            Assert.NotEqual(a, null);
+            Assert.NotEqual(null, a);
+        }
+
+        [Fact]
+        public void TestCompareOperands()
+        {
+            var a = new OpenCollar.Azure.ReliableQueue.Model.Topic("AAA");
+            var a1 = new OpenCollar.Azure.ReliableQueue.Model.Topic("AAA");
+            var b = new OpenCollar.Azure.ReliableQueue.Model.Topic("BBB");
+            var c = new OpenCollar.Azure.ReliableQueue.Model.Topic("CCC");
+
+            Assert.True(a == a);
+            Assert.True(a == a1);
+            Assert.False(a > a);
+            Assert.True(a >= a);
+
+            Assert.False(a != a);
+            Assert.False(a < a);
+            Assert.True(a <= a);
+
+            Assert.True(a != b);
+            Assert.False(a == b);
+            Assert.True(a < b);
+            Assert.True(a <= b);
+        }
+
+        [Fact]
+        public void TestCompareTo()
+        {
+            var a = new OpenCollar.Azure.ReliableQueue.Model.Topic("AAA");
+            var a1 = new OpenCollar.Azure.ReliableQueue.Model.Topic("AAA");
+            var b = new OpenCollar.Azure.ReliableQueue.Model.Topic("BBB");
+            var c = new OpenCollar.Azure.ReliableQueue.Model.Topic("CCC");
+
+            Assert.Equal(1, a.CompareTo((OpenCollar.Azure.ReliableQueue.Model.Topic)null));
+            Assert.Equal(0, a.CompareTo(a));
+            Assert.Equal(0, a.CompareTo(a1));
+            Assert.Equal(-1, a.CompareTo(b));
+            Assert.Equal(1, b.CompareTo(a));
+        }
+
+        [Fact]
+        public void TestCompareToObject()
+        {
+            var a = new OpenCollar.Azure.ReliableQueue.Model.Topic("AAA");
+            var a1 = new OpenCollar.Azure.ReliableQueue.Model.Topic("AAA");
+            var b = new OpenCollar.Azure.ReliableQueue.Model.Topic("BBB");
+            var c = new OpenCollar.Azure.ReliableQueue.Model.Topic("CCC");
+
+            Assert.Equal(1, a.CompareTo((object)null));
+            Assert.Equal(0, a.CompareTo((object)a));
+            Assert.Equal(0, a.CompareTo((object)a1));
+            Assert.Equal(-1, a.CompareTo((object)b));
+            Assert.Equal(1, b.CompareTo((object)a));
+            Assert.Throws<ArgumentException>(() => a.CompareTo(222));
+        }
+
+        [Fact]
+        public void TestConstructor()
+        {
+            var x = new OpenCollar.Azure.ReliableQueue.Model.Topic("TOPIC+NAME+1");
+            Assert.NotNull(x);
+            Assert.Equal("topic-name-1", x.Identifier);
+            Assert.Equal("TOPIC+NAME+1", x.ToString());
+            Assert.False(x.IsEmpty);
+
+            x = new OpenCollar.Azure.ReliableQueue.Model.Topic(null);
+            Assert.NotNull(x);
+            Assert.Equal(OpenCollar.Azure.ReliableQueue.Model.Topic.Default, x);
+            Assert.Equal(OpenCollar.Azure.ReliableQueue.Model.Topic.DefaultIdentifier, x.Identifier);
+            Assert.True(x.IsEmpty);
+
+            x = new OpenCollar.Azure.ReliableQueue.Model.Topic(string.Empty);
+            Assert.NotNull(x);
+            Assert.Equal(OpenCollar.Azure.ReliableQueue.Model.Topic.Default, x);
+            Assert.Equal(OpenCollar.Azure.ReliableQueue.Model.Topic.DefaultIdentifier, x.Identifier);
+            Assert.True(x.IsEmpty);
+
+            x = new OpenCollar.Azure.ReliableQueue.Model.Topic(" \r\n\t");
+            Assert.NotNull(x);
+            Assert.Equal(OpenCollar.Azure.ReliableQueue.Model.Topic.Default, x);
+            Assert.Equal(OpenCollar.Azure.ReliableQueue.Model.Topic.DefaultIdentifier, x.Identifier);
+            Assert.True(x.IsEmpty);
+        }
+
+        [Fact]
+        public void TestEquals()
+        {
+            var a = new OpenCollar.Azure.ReliableQueue.Model.Topic("AAA");
+            var a1 = new OpenCollar.Azure.ReliableQueue.Model.Topic("AAA");
+            var b = new OpenCollar.Azure.ReliableQueue.Model.Topic("BBB");
+            var c = new OpenCollar.Azure.ReliableQueue.Model.Topic("CCC");
+
+            Assert.False(a.Equals((OpenCollar.Azure.ReliableQueue.Model.Topic)null));
+            Assert.True(a.Equals(a));
+            Assert.True(a.Equals(a1));
+            Assert.False(a.Equals(22));
+            Assert.False(a.Equals(b));
+        }
+
+        [Fact]
+        public void TestEqualsObjects()
+        {
+            var a = new OpenCollar.Azure.ReliableQueue.Model.Topic("AAA");
+            var a1 = new OpenCollar.Azure.ReliableQueue.Model.Topic("AAA");
+            var b = new OpenCollar.Azure.ReliableQueue.Model.Topic("BBB");
+            var c = new OpenCollar.Azure.ReliableQueue.Model.Topic("CCC");
+
+            Assert.False(a.Equals((object)null));
+            Assert.True(a.Equals((object)a));
+            Assert.True(a.Equals((object)a1));
+            Assert.False(a.Equals((object)22));
+            Assert.False(a.Equals((object)b));
+        }
+
+        [Fact]
+        public void TestToString()
+        {
+            var a = new OpenCollar.Azure.ReliableQueue.Model.Topic("AAA");
+            var a1 = new OpenCollar.Azure.ReliableQueue.Model.Topic("AAA");
+            var b = new OpenCollar.Azure.ReliableQueue.Model.Topic("BBB");
+
+            Assert.Equal(a.GetHashCode(), a.GetHashCode());
+            Assert.Equal(a.GetHashCode(), a1.GetHashCode());
+            Assert.NotEqual(a.GetHashCode(), b.GetHashCode());
+        }
+    }
+}
